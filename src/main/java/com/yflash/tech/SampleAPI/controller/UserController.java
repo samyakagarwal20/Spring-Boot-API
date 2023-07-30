840c7d4512ccb19459e77bde22522f7d15b0acb2
samyak <samyakagarwal20@gmail.com>
2023-07-30 13:37:39 +0530

    adding changes to all API for User

--- src/main/java/com/yflash/tech/SampleAPI/controller/UserController.java
+++ src/main/java/com/yflash/tech/SampleAPI/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.yflash.tech.SampleAPI.controller;
 
 import com.yflash.tech.SampleAPI.entity.UserEntity;
+import com.yflash.tech.SampleAPI.model.in.DeleteUserRequest;
 import com.yflash.tech.SampleAPI.model.in.GetUserRequest;
+import com.yflash.tech.SampleAPI.model.in.PostUserRequest;
+import com.yflash.tech.SampleAPI.model.in.PutUserRequest;
 import com.yflash.tech.SampleAPI.model.out.User;
 import com.yflash.tech.SampleAPI.service.UserService;
 import org.apache.logging.log4j.LogManager;
@@ -9,10 +12,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +33,24 @@ public class UserController {
     @GetMapping(value = "/get-user-by-id", produces = "application/json", consumes = "application/json")
     ResponseEntity<User> getUserById(@RequestBody GetUserRequest userRequest) {
         return new ResponseEntity<>(userService.getUserById(userRequest.getId()), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/add-user", produces = "application/json", consumes = "application/json")
+    public ResponseEntity<User> addUserDetails(@RequestBody PostUserRequest userRequest) {
+        return new ResponseEntity<>(userService.addUserDetails(userRequest), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/update-user-details", produces = "application/json", consumes = "application/json")
+    public ResponseEntity<User> updateUserDetails(@RequestBody PutUserRequest userRequest) {
+        return new ResponseEntity<>(userService.updateUserDetails(userRequest), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete-user", produces = "text/plain", consumes = "application/json")
+    public ResponseEntity<String> deleteUserDetails(@RequestBody DeleteUserRequest userRequest) {
+        String serviceResponse = userService.deleteUserDetails(userRequest.getId());
+        if(serviceResponse == null)
+            return new ResponseEntity<>("Data doesn't exists for this id", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("Data for id " + serviceResponse + " is deleted successfully!", HttpStatus.OK);
     }
 
 }
--- src/main/java/com/yflash/tech/SampleAPI/service/impl/UserServiceImpl.java
+++ src/main/java/com/yflash/tech/SampleAPI/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.yflash.tech.SampleAPI.service.impl;
 
 import com.yflash.tech.SampleAPI.entity.UserEntity;
+import com.yflash.tech.SampleAPI.model.in.DeleteUserRequest;
+import com.yflash.tech.SampleAPI.model.in.PostUserRequest;
+import com.yflash.tech.SampleAPI.model.in.PutUserRequest;
 import com.yflash.tech.SampleAPI.model.out.User;
 import com.yflash.tech.SampleAPI.repository.UserRepository;
 import com.yflash.tech.SampleAPI.service.UserService;
@@ -32,5 +35,25 @@ public class UserServiceImpl implements UserService {
     public User getUserById(Integer id) {
         UserEntity userEntity = userRepository.getReferenceById(id);
         return modelMapper.map(userEntity,User.class);
+    }
+    @Override
+    public User addUserDetails(PostUserRequest userRequest) {
+        UserEntity userEntity = modelMapper.map(userRequest,UserEntity.class);
+        userEntity = userRepository.save(userEntity);
+        return modelMapper.map(userEntity, User.class);
+    }
+    @Override
+    public User updateUserDetails(PutUserRequest userRequest) {
+        UserEntity userEntity = modelMapper.map(userRequest,UserEntity.class);
+        userEntity = userRepository.save(userEntity);
+        return modelMapper.map(userEntity, User.class);
+    }
+    @Override
+    public String deleteUserDetails(Integer id) {
+        if(userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return id.toString();
+        }
+        return null;
     }
 }
